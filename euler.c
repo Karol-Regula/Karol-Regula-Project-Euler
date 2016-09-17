3e003f551595ab1b781c57d1944c32b1acfedf99
Karol-Regula <kregula@stuy.edu>
2016-09-17 14:13:09 -0400

    Finished solution for Euler Problem 2.

--- euler.c
+++ euler.c
@@ -4,37 +4,48 @@
 
 
 int problem1(){
-  int x = 0;
-  int total = 0;
-  while (x < 1000){
-    if (x % 3 == 0 || x % 5 == 0){
-      total += x;
-      //printf("%d\n",x);
+    int x = 0;
+    int total = 0;
+    while (x < 1000){
+	if (x % 3 == 0 || x % 5 == 0){
+	    total += x;
+	    //printf("%d\n",x);
+	}
+	x++;
     }
-    x++;
-  }
-  return total;
+    return total;
 }
 
 int problem2(){
-  int x1 = 1;
-  int x2 = 2;
-  int total = 2;
-  while (x1 < 4000000 && x2 < 4000000){
-  }
-  return total;
+    int x1 = 1;
+    int x2 = 2;
+    int total = 2;
+    while (x1 < 4000000 && x2 < 4000000){
+	if (x2 > x1){
+	    x1 = x1 + x2;
+	    if (x1 % 2 == 0){
+		total += x1;
+	    }
+	}else{
+	    x2 = x1 + x2;
+	    if (x2 % 2 ==0){
+		total += x2;
+	    }
+	}
+	//printf("%d, %d \n", x1, x2);
+    }
+    return total;
 }
 
 int main(){
-  printf("This works? \n");
-  
-  int p1 = problem1();
-  printf("Problem 1: %d \n", p1);
 
-  //int p2 = problem2();
-  //printf("Problem 2: %d \n", p2);
+    int p1 = problem1();
+    printf("Problem 1: %d \n", p1);
+
+    int p2 = problem2();
+    printf("Problem 2: %d \n", p2);
   
-  return 0;
+    return 0;
 }
 
 
